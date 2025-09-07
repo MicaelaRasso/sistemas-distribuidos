f560831345ab519123077e853aaffcaba3a117f1
Micaela <micaelarasso99@gmail.com>
2025-09-07 21:35:31 +0000

    entrega tp2

---
+++ TP2/index.js
@@ -0,0 +1,61 @@
+const axios = require('axios');
+
+async function obtenerUsuario(id) {
+
+  try {
+
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+    //console.log(`Nombre: ${response.data.name}`);
+    return response.data.name;
+  
+  } catch (error) {
+
+    console.error('Error al obtener el usuario:', error.message);
+
+  }
+}
+
+async function obtenerPublicaciones(id) {
+
+  try {
+
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+    //console.log(`Publicaciones: ${response.data}`);
+    //console.log(`Publicaciones: ${response.data.length}`);
+    return response.data.length;
+
+  } catch (error) {
+     console.error('Error al obtener el usuario:', error.message);
+
+  }
+
+}
+
+
+console.log(`--- Ejecución Secuencial ---`);
+async function Secuencial() {
+  for (let i = 1; i <= 3; i++) {
+    const usuario = await obtenerUsuario(i);
+    const publicaciones = await obtenerPublicaciones(i); 
+    console.log(`${usuario} tiene ${publicaciones} publicaciones`);
+  }
+}
+Secuencial();
+
+console.log(`--- Ejecución Paralela ---`);
+async function main() {
+  const promesas = [];
+
+  for (let i = 1; i <= 3; i++) {
+    promesas.push(
+      Promise.all([obtenerUsuario(i), obtenerPublicaciones(i)])
+    );
+  }
+
+  const resultados = await Promise.all(promesas);
+
+  resultados.forEach(([usuario, publicaciones]) => {
+    console.log(`${usuario} tiene ${publicaciones} publicaciones`);
+  });
+}
